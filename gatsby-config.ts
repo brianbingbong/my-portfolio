6b40bc94b0875d2ebf6bf2404d96266f21967abd
Brian Li Ong <brian.ong1999@gmail.com>
2022-06-13 11:40:05 +1000

    point config file to point to correct gatsby icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,9 @@ import { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Brian Ong`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@Brian Ong`,
+    author: `@Brian Li Ong`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   graphqlTypegen: true,
@@ -33,7 +33,7 @@ const config: GatsbyConfig = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/content/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
