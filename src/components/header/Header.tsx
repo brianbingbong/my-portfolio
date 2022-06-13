33bd24a22fb562c44d045089bee163c4b7b873eb
Brian Li Ong <brian.ong1999@gmail.com>
2022-06-13 12:06:42 +1000

    add blog page and blog link in nav bar header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
-import * as styles from "./Header.module.scss";
+import * as styles from "./Header.module.scss"
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link to="/" aria-label="home">
-        <div className={styles.logo}>km.</div>
-      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" aria-label="home">
+              <div className={styles.logo}>km.</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog" aria-label="blog">
+              <div className={styles.headerLink}>blog</div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
---
+++ src/pages/blog.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/layout/Layout"
+
+const BlogPage = () => {
+  return <Layout>
+    <p>This is where my blog posts wil go</p>
+  </Layout>
+}
+
+export default BlogPage
